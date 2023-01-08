cedab65fec405c9bccac5747bf5f8bbae50f4650
thegreentimtam <tim@timgreenfilms.com.au>
2023-01-09 06:13:40 +0800

    PyPi - ify

---
+++ setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="ffAlive",
+    version="1.0.0",
+    author="Tim Green",
+    author_email="the.green.timtam@gmail.com",
+    description="FFMPEG with Progress Bars",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/packagename",
+    packages=setuptools.find_packages(),
+    install_requires  = ['subprocess', 'json', 'alive_progress', 'ffmpeg_progress_yield' ],
+    license = 'MIT'
+)
